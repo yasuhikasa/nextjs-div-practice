33bed897c779dff707bba4810c45edfc3636ec53
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-07-24 21:06:54 +0900

    Revert "Revert "お問い合わせuseContext実装""
    This reverts commit 9774621840a4c5cb3bbc0376a7807dcd17ec16d8.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,17 @@ import type { AppProps } from 'next/app'
 import React from 'react';
 import { useEffect } from 'react';
 import Modal from 'react-modal';
+import { FormDataProvider } from './context/formDataProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     Modal.setAppElement('#__next');
   }, []);
-  return <Component {...pageProps} />
+  
+  return (
+    <FormDataProvider>
+      <Component {...pageProps} />
+    </FormDataProvider>
+  );
 }
