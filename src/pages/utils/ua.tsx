6e9bc822c7bfca13f59ea854ab609bcb185b01dc
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-10-08 19:48:42 +0900

    ユーザーエイジェントの判定とローカルホストの判定を追加

---
+++ src/pages/utils/ua.tsx
@@ -0,0 +1,95 @@
+/*-------------------------------------------------------------
+
+UIパーツ共通関数
+-------------------------------------------------------------*/
+// UAの固定値
+const selfMonitar = "AiSEG2" // 自モニタ
+const home = "HomeCTRL" // ホームナビ
+const door = "DoorCTRL" // ドアホン
+const cloudge = "PEWDMSHomeViewer" // クラウジュ
+/**
+
+UAを受け取って、自モニタかどうか判断する関数
+@param {string} ua UserAgent
+@returns {boolean} true:自モニタ false:自モニタ以外
+*/
+export function judgeMonitor(ua: string): boolean {
+if (ua.match(selfMonitar) || ua.match(home)) {
+return true
+}
+return false
+}
+
+
+/****************************************************
+
+ユーティリティ ライブラリ（認証サーバ全体で使用する関数を定義）
+@module util
+*****************************************************/
+import crypto from "crypto"
+import ipaddr from "ipaddr.js"
+import { execSync } from "child_process"
+// デフォルトゲートウェイのIPアドレス
+let defaultGateWayIpAddress = "0.0.0.0"
+
+/**
+
+MD5変換関数
+@param {string} word MD5変換対象の文字列
+@example
+md5("abc") => "900150983cd24fb0d6963f7d28e17f72"
+*/
+export const md5 = (word: string) => crypto.createHash("MD5").update(word).digest("hex")
+/**
+
+ローカルホスト判定
+@param {any} request リクエストオブジェクト
+@return {boolean} ローカルホスト判定(true/false)
+*/
+export const isLocalhost = function (request: any) {
+const ip = getClientIp(request)
+const ipv4Addr = cnvIpv4MappedAddressToIpv4Address(ip)
+return ipv4Addr.indexOf(defaultGateWayIpAddress) > -1
+}
+/**
+
+クライアントIP取得
+@param {any} request リクエストオブジェクト
+@return {any} クライアントIPアドレス
+*/
+export const getClientIp = function (request: any) {
+return request.connection && request.connection.remoteAddress
+? request.connection.remoteAddress
+: request.connection.socket && request.connection.socket.remoteAddress
+? request.connection.socket.remoteAddress
+: request.socket && request.socket.remoteAddress
+? request.socket.remoteAddress
+: "0.0.0.0"
+}
+/**
+
+IPv4射影アドレス変換処理
+@param {string} ipv4MappedAddr IPv4射影アドレス
+@returns {string} IPv4アドレス
+*/
+export const cnvIpv4MappedAddressToIpv4Address = (ipv4MappedAddr: string): string => {
+let ipv4Addr = "0.0.0.0"
+if (ipaddr.IPv6.isIPv6(ipv4MappedAddr) === true) {
+const addr = ipaddr.IPv6.parse(ipv4MappedAddr)
+ipv4Addr = addr.toIPv4Address().toString()
+}
+return ipv4Addr
+}
+/**
+
+デフォルトゲートウェイ IPアドレス取得
+起動時に実行し取得したIPアドレスを変数に保持する
+*/
+function getDefaultGateWayIpAddress() {
+const utf8decorder = new TextDecoder()
+const result = execSync("ip route show")
+const nets = utf8decorder.decode(result).split(" ")
+defaultGateWayIpAddress = nets[2]
+}
+// 起動時に実行
+getDefaultGateWayIpAddress()
